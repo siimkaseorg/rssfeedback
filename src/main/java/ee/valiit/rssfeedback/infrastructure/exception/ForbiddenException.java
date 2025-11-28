d13aec1d89aa5d74b3a961be4e71ca8903a232d7
Siim <siim.kaseorg@gmail.com>
2025-11-28 11:52:29 +0200

    added infrastructure

---
+++ src/main/java/ee/valiit/rssfeedback/infrastructure/exception/ForbiddenException.java
@@ -0,0 +1,15 @@
+package ee.valiit.rssfeedback.infrastructure.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ForbiddenException extends RuntimeException {
+    private final String message;
+    private final Integer errorCode;
+
+    public ForbiddenException(String message, Integer errorCode) {
+        super(message);
+        this.message = message;
+        this.errorCode = errorCode;
+    }
+}
