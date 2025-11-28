1efd4df99ee6b95e366417cb13da0e8097932a7c
Siim <siim.kaseorg@gmail.com>
2025-11-28 11:42:04 +0200

    initial commit

---
+++ src/main/java/ee/valiit/rssfeedback/RssfeedbackApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.rssfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssfeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssfeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/rssfeedback/RssfeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.rssfeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssfeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
