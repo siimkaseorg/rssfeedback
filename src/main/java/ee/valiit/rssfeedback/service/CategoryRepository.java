8660bbe75d867e83f6a2267e982477adf6f09bd0
Siim <siim.kaseorg@gmail.com>
2025-12-03 13:22:12 +0200

    login done?

--- src/main/java/ee/valiit/rssfeedback/service/CategoryRepository.java
+++ src/main/java/ee/valiit/rssfeedback/service/CategoryRepository.java
@@ -2,6 +2,7 @@ package ee.valiit.rssfeedback.service;
 
 import ee.valiit.rssfeedback.persitence.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
